2e0f0bdfb2f1cfc31f164ac63c1dc2283581d07f
gabriel221217 <123956109+Gabriel08081998@users.noreply.github.com>
2023-10-21 13:09:09 -0300

    Update ContaTerminal.java
    Nesta versão aprimorada, o código foi reestruturado em métodos para tornar o código mais modular e legível. Foram adicionadas mensagens informativas ao usuário para orientar o processo de entrada de dados e validar as informações inseridas. Além disso, o código utiliza nomes de variáveis ​​e métodos mais descritivos para melhorar a compreensão.

--- src/contabanco/ContaTerminal.java
+++ src/contabanco/ContaTerminal.java
@@ -1,85 +1,78 @@
 package contabanco;
 
-
 import java.util.Scanner;
 
-import javax.swing.event.DocumentEvent.EventType;
+public class ContaBancaria {
 
-public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-	public static void main(String[] args) {
-	
-		
-		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("Por favor, informe os dados da conta!");
-		
-		
-		boolean verificarConta = false;
-		String numero = "";
-		while (!verificarConta) {
-			
-			try {
-				System.out.println("Informe a Conta com 8 dígitos!");
-				numero = entrada.next();
-				Integer.valueOf(numero);
-				verificarConta = true;
-			
-			} catch (Exception e) {
-				System.out.println("Digite somente números!");
-				verificarConta = false;
-			}
-			
-			if (numero.length() != 8) {
-				System.out.println("A conta precisa ter 8 digitos!");
-				verificarConta = false;
-			}
-			
-		}
-		
-		boolean verificarAgencia = false;
-		String agencia ="";
-		while (!verificarAgencia) {
-			
-			try {
-				System.out.println("Informe o numero da agencia de 4 digitos!");
-				agencia = entrada.next();
-				Integer.valueOf(agencia);
-				verificarAgencia = true;
-				
-			} catch (Exception e) {
-				System.out.println("Digite somente números!");
-				verificarAgencia = false;
-			}
-			
-			if (agencia.length() != 4) {
-				System.out.println("A agencia precisa ter 4 digitos");
-				verificarAgencia=false;
-			}
-		 }
-		
-		entrada.nextLine();
-		
-		boolean verificarNome = false;
-		String nomeCliente = "";
-		while (!verificarNome) {
-			System.out.println("Informe nome do usuario!");
-			 nomeCliente =entrada.nextLine();
-			 
-			 if(!nomeCliente.matches(".*\\d+.*")) {
-				 verificarNome = true;
-				
-			}else {
-				System.out.println("Nome nao pode conter numeros");	
-			}	
-		}
-		double saldo = 5000.00;
-		
-		entrada.close(); 
-		
-		System.out.println("Olá " + nomeCliente + " obrigado por cria uma conta em nosso banco, Sua agancia é " 
-		+ agencia +", Conta " + numero + "e seu saldo " + saldo + " já está disponivel para saque.");
-		
-		
-	}
+        System.out.println("Bem-vindo ao nosso banco!");
+        String numeroConta = obterNumeroConta(entrada);
+        String numeroAgencia = obterNumeroAgencia(entrada);
+        String nomeCliente = obterNomeCliente(entrada);
+
+        double saldoInicial = 5000.00;
+
+        entrada.close();
+
+        System.out.println("Olá, " + nomeCliente + "! Sua conta no banco foi criada com sucesso.");
+        System.out.println("Agência: " + numeroAgencia);
+        System.out.println("Conta: " + numeroConta);
+        System.out.println("Saldo inicial: R$" + saldoInicial);
+    }
+
+    private static String obterNumeroConta(Scanner entrada) {
+        String numeroConta;
+        boolean contaValida = false;
+
+        do {
+            System.out.print("Informe o número da conta com 8 dígitos: ");
+            numeroConta = entrada.next();
+
+            if (numeroConta.matches("\\d{8}")) {
+                contaValida = true;
+            } else {
+                System.out.println("Número de conta inválido. Deve conter 8 dígitos.");
+            }
+        } while (!contaValida);
+
+        return numeroConta;
+    }
+
+    private static String obterNumeroAgencia(Scanner entrada) {
+        String numeroAgencia;
+        boolean agenciaValida = false;
+
+        do {
+            System.out.print("Informe o número da agência com 4 dígitos: ");
+            numeroAgencia = entrada.next();
+
+            if (numeroAgencia.matches("\\d{4}")) {
+                agenciaValida = true;
+            } else {
+                System.out.println("Número de agência inválido. Deve conter 4 dígitos.");
+            }
+        } while (!agenciaValida);
+
+        return numeroAgencia;
+    }
+
+    private static String obterNomeCliente(Scanner entrada) {
+        String nomeCliente;
+        boolean nomeValido = false;
+
+        do {
+            System.out.print("Informe o nome do cliente: ");
+            nomeCliente = entrada.next();
+
+            if (!nomeCliente.matches(".*\\d+.*")) {
+                nomeValido = true;
+            } else {
+                System.out.println("O nome não pode conter números.");
+            }
+        } while (!nomeValido);
+
+        return nomeCliente;
+    }
 }
